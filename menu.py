9b5a5c2905362ec2975f2e6e08e876ba35b05b88
dmoreira1 <dmoreira@uade.edu.ar>
2024-09-14 18:16:20 -0300

    Menu y submenu

---
+++ menu.py
@@ -0,0 +1,32 @@
+def mostrar_menu():
+    print("\nAplicación de Seguimiento de Películas y Series Vistas")
+    print("Menú Principal")
+    print("1. Usuarios")
+    print("2. Películas/Series")
+    print("3. Registros Vistos")
+    print("4. Salir")
+
+def mostrar_submenu(opcion):
+    if opcion == '1':
+        print("Usuarios")
+        print("a. Agregar Usuario")
+        print("b. Listar Usuarios")
+        print("c. Actualizar Usuario")
+        print("d. Eliminar Usuario")
+        print("e. Generar reporte")
+        print("f. Volver al Menú Principal")
+    elif opcion == '2':
+        print("Películas/Series")
+        print("a. Agregar Película/Serie")
+        print("b. Listar Películas/Series")
+        print("c. Actualizar Película/Serie")
+        print("d. Eliminar Película/Serie")
+        print("e. Generar reporte")
+        print("f. Volver al Menú Principal")
+    elif opcion == '3':
+        print("Registros Vistos")
+        print("a. Agregar Registro")
+        print("b. Listar Registros")
+        print("c. Actualizar Registro")
+        print("e. Generar reporte")
+        print("f. Volver al Menú Principal")
